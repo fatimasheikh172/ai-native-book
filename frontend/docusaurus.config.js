5a38c5ce3eee6e614707439663ce473abcff209a
githubusername <githubemail>
2025-12-25 13:34:00 +0500

    updated

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-username.github.io',
+  url: 'https://github.com/fatimasheikh172/ai-native-book',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
   baseUrl: '/ai-native-book',
